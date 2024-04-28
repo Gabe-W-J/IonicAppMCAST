bbd89b5f4daf884a1a65d3b476984e7f2e9421a3
Gabriel.W.Jackson <137659740+Gabe-W-J@users.noreply.github.com>
2024-04-28 21:02:31 +0200

    Add files via upload

---
+++ master/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Gabriel Jackson MSD 4.2A',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
